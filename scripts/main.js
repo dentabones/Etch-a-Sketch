773ddfa34503ef985fb3ad2d31873087fdf15645
dentabones <sh183610@gmail.com>
2022-12-13 12:33:07 -0500

    Add color change functions

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,28 @@ const black = document.querySelector(".black");
 const color = document.querySelector(".color");
 let allDivs = containerEl.querySelectorAll(".littlegrid");
 let currentSize;
+
+function createDivs(num) {
+  for (let i = 0; i < num; i++) {
+    let childDiv = document.createElement("div");
+
+    black.addEventListener("click", function () {
+      childDiv.addEventListener("mouseover", setBlackColor);
+    });
+
+    color.addEventListener("click", function () {
+      childDiv.addEventListener("mouseover", setGridColor);
+    });
+    // childDiv.addEventListener("mouseover", setGridColor);
+    containerEl.appendChild(childDiv);
+  }
+}
+
+const setGridColor = (e) => {
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  e.target.style.backgroundColor = "#" + randomColor;
+};
+
+const setBlackColor = (e) => {
+  e.target.style.backgroundColor = "#000000";
+};
